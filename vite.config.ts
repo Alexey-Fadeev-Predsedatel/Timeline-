02921ff387d8024ffa93f0cca68e53a5700e500a
Alexey-Fadeev-Predsedatel <koeningerkonyn@gmail.com>
2026-08-10 15:22:03 +0300

    фпфшт

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/Timeline-/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
